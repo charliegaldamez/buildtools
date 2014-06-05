df8db58915393b20fa01cd23a654763806190c58
Jurian Even <jurian@twentronix.com>
2014-06-05 13:58:05 +0200

    Refactored MinifyTask and added support for merging files

--- phingext/MinifyTask.php
+++ phingext/MinifyTask.php
@@ -17,9 +17,13 @@ require_once 'phing/Task.php';
  * This task requires YUI compressor. You can install it via the terminal with
  * the command "brew install yuicompressor".
  *
+ * Merging multiple files of a fileset into one file is also supported. This
+ * can be done by setting the 'tofile' property. Be aware that the fileset should
+ * only contain js OR css files.
+ *
  * Example (replace \/ by /):
  * <code>
- *  <minify>
+ *  <minify watcherfilepath="${watcher.file.path}" tofile="${dirs.plugins}/system/cookieconfirm/media/js/combined.min.js">
  *		<fileset dir="${dirs.plugins}/system/cookieconfirm/media">
  *			<include name="*.js" />
  *			<include name="**\/*.js" />
@@ -32,7 +36,7 @@ require_once 'phing/Task.php';
 class MinifyTask extends Task
 {
 	/**
-	 * Set to true to output messages and warnings
+	 * Set to true to output messages and warnings.
 	 *
 	 * @var bool
 	 */
@@ -40,14 +44,22 @@ class MinifyTask extends Task
 
 	/**
 	 * Collection of filesets
-	 * Used when linking contents of a directory
+	 *
+	 * Used when linking contents of a directory.
 	 *
 	 * @var array
 	 */
 	private $filesets = array();
 
 	/**
-	 * The file which is triggered by PHP Storm's File Watcher
+	 * The destination file.
+	 *
+	 * @var null
+	 */
+	protected $toFile = null;
+
+	/**
+	 * The file which is triggered by PHP Storm's File Watcher.
 	 *
 	 * @var null
 	 */
@@ -123,7 +135,13 @@ class MinifyTask extends Task
 			{
 				if (!empty($target))
 				{
-					$targets[$target] = realpath($fromDir . DIRECTORY_SEPARATOR . $target);
+					$targetPath = realpath($fromDir . DIRECTORY_SEPARATOR . $target);
+
+					// Do not add the temp merge filepath to the map
+					if ($targetPath !== $this->getTempMergeFilePath())
+					{
+						$targets[$target] = $targetPath;
+					}
 				}
 			}
 		}
@@ -132,11 +150,50 @@ class MinifyTask extends Task
 	}
 
 	/**
+	 * Gets the destination file.
+	 *
+	 * @return  object  PhingFile  The destination file. Either a string or a PhingFile object.
+	 */
+	public function getTofile()
+	{
+		return $this->toFile;
+	}
+
+	/**
+	 * Gets the absolute destination file path.
+	 *
+	 * @return  null|string  The absolute destination file path or 'null' if 'to file' is not provided.
+	 */
+	protected function getTofilePath()
+	{
+		$toFilePath = null;
+
+		$toFile = $this->getTofile();
+
+		if ($toFile)
+		{
+			$toFilePath = $toFile->getAbsolutePath();
+		}
+
+		return $toFilePath;
+	}
+
+	/**
+	 * Sets the destination file.
+	 *
+	 * @param  object  PhingFile  The destination file. Either a string or a PhingFile object.
+	 */
+	public function setTofile(PhingFile $file)
+	{
+		$this->toFile = $file;
+	}
+
+	/**
 	 * Sets the file which is triggered by PHP Storm's File Watcher.
 	 *
-	 * @param  PhingFile  The source file. Either a string or an PhingFile object
+	 * @param  object  PhingFile  The source file. Either a string or a PhingFile object.
 	 */
-	function setWatcherFilePath(PhingFile $file)
+	public function setWatcherFilePath(PhingFile $file)
 	{
 		$this->watcherFilePath = $file;
 	}
@@ -146,49 +203,240 @@ class MinifyTask extends Task
 	 *
 	 * @throws BuildException
 	 */
-	function main()
+	public function main()
 	{
+		// Get the files from the fileset
+		$files = array();
 		$map = $this->getMap();
 
-		if (is_file($this->watcherFilePath))
+		foreach ($map as $file)
 		{
-			// Only minify the file if it's in the fileset
-			if (in_array($this->watcherFilePath, $map))
+			$files[] = $file;
+
+			if ($this->needToMergeFiles())
 			{
-				$this->minify($this->watcherFilePath);
+				echo "\t\t\t  - $file" . PHP_EOL;
 			}
+		}
+
+		if ($this->needToMergeFiles())
+		{
+			$mergedFile = $this->mergeFiles($files);
+			$this->minifyFile($mergedFile, true);
 		}
 		else
 		{
-			foreach ($map as $file)
+			// If it's a watcherfilepath file(?) then get the files of that path to minify?
+			if (is_file($this->watcherFilePath) && !$this->needToMergeFiles())
 			{
-				$this->minify($file);
+				if (in_array($this->watcherFilePath, $map))
+				{
+					$files = array($this->watcherFilePath);
+				}
 			}
-		}	
-		
+
+			$this->minifyFiles($files);
+		}
+
+		$this->deleteTempMergeFile();
+
 		return true;
 	}
 
 	/**
-	 * Minifies a file using YUI compressor.
+	 * Checks if we need to merge the files.
 	 *
-	 * @param $file
+	 * If the 'tofile' property has been set in the Phing XML build file, we
+	 * need to merge the files into one file.
+	 *
+	 * @return  boolean  True if we need to merge the files.
+	 */
+	private function needToMergeFiles()
+	{
+		$toFile = $this->getToFile();
+
+		return isset($toFile);
+	}
+
+	/**
+	 * Merges files into one file.
+	 *
+	 * All file extensions should be the same in order to minify them into one file.
+	 *
+	 * @param  array  $files  The files to merge.
+	 *
+	 * @return  string|false  The path to the merged file or false if 'tofile' is not set
+	 * 						  in the XML Phing build file.
 	 *
 	 * @throws BuildException
 	 */
-	protected function minify($file)
+	private function mergeFiles($files)
 	{
-		echo "\t\t\t$file\n";
+		// Delete the temp file to make sure its contents are not (recursively) added to the temporary merge file
+		$this->deleteTempMergeFile();
 
-		$ext = pathinfo($file, PATHINFO_EXTENSION);
+		$mergeFilePath = $this->getTempMergeFilePath();
 
-		if (in_array($ext, array('css', 'js')))
+		if (!$mergeFilePath)
 		{
-			exec("yuicompressor ".escapeshellarg($file)." -o '.".$ext."$:.min.".$ext."' ".escapeshellarg($file)." ".($this->debug ? '-v' : '')." --line-break 5000 --charset utf-8");
+			return false;
+		}
+
+		// Make sure all files have the same extension (you can't merge .css and .js to one file and minify it...)
+		$extensions = $this->getFilesExtensions($files);
+
+		foreach ($extensions as $extension)
+		{
+			if ($extension != $extensions[0])
+			{
+				throw new BuildException("All file extensions of a single minify task should be the same "
+					. "($extension != $extensions[0]) when the files are going to be merged ('tofile' is "
+					. "set in the Phing build file).");
+			}
+		}
+
+		$data = array();
+
+		foreach ($files as $filename)
+		{
+			$data[] = file_get_contents($filename);
+		}
+
+		file_put_contents($mergeFilePath, $data);
+
+		return $mergeFilePath;
+	}
+
+
+	/**
+	 * Gets the path to the temporary merge file.
+	 *
+	 * This file is used for merging .css or .js files.
+	 *
+	 * @return  string|false  The path to the temporary merge file or false if
+	 * 						  'tofile' is not set in the XML Phing build file.
+	 */
+	private function getTempMergeFilePath()
+	{
+		if (!$this->getTofilePath())
+		{
+			return false;
+		}
+
+		return $this->getTofilePath() . '.tempmerge';
+	}
+
+	/**
+	 * Deletes the temporary merged file.
+	 */
+	private function deleteTempMergeFile()
+	{
+		$tempMergeFilePath = $this->getTempMergeFilePath();
+
+		if (is_file($tempMergeFilePath))
+		{
+			unlink($tempMergeFilePath);
+		}
+	}
+
+	/**
+	 * Minifies file(s) using YUI compressor.
+	 *
+	 * @param  array  $files
+	 *
+	 * @throws BuildException
+	 */
+	private function minifyFiles($files)
+	{
+		foreach ($files as $file)
+		{
+			$this->minifyFile($file, false);
+		}
+	}
+
+	/**
+	 * Minifies file(s) using YUI compressor.
+	 *
+	 * @param  array    $file          The file to minify.
+	 * @param  boolean  $isMergedFile  True if it's a merged file.
+	 *
+	 * @throws BuildException
+	 */
+	private function minifyFile($file, $isMergedFile)
+	{
+		if ($isMergedFile)
+		{
+			$cleanedFile = rtrim($file, '.tempmerge');
 		}
 		else
 		{
-			throw new BuildException('Invalid file extension!');
+			$cleanedFile = $file;
 		}
+
+		$extension = $this->getFileExtension($cleanedFile);
+
+		if ($isMergedFile)
+		{
+			$outputFileArgument = rtrim($file, '.tempmerge');
+			$outputFilePath = $outputFileArgument;
+		}
+		else
+		{
+			// minify all .css / .js files and save them as .min.css or .min.js
+			$outputFileArgument = "." . $extension . "$:.min." . $extension;
+			$outputFilePath = str_replace(array('.css', '.js'), array('.min.css', '.min.js'), $file);
+
+			echo "\t\t\t  - $cleanedFile" . PHP_EOL;
+		}
+
+		echo "\t\t\t -> $outputFilePath" . PHP_EOL;
+
+		exec("yuicompressor " . escapeshellarg($file)
+			. " --type " . $extension
+			. " -o '". escapeshellarg($outputFileArgument) . "' "
+			. ($this->debug ? ' -v' : '')
+			. " --line-break 5000 --charset utf-8");
+	}
+
+	/**
+	 * Gets the file extensions.
+	 *
+	 * @param   array    $files        Files to get the extensions from.
+	 *
+	 * @return  array  The files extensions.
+	 *
+	 * @throws  BuildException
+	 */
+	private function getFilesExtensions($files)
+	{
+		$extensions = array();
+
+		foreach ($files as $file)
+		{
+			$extensions[] = $this->getFileExtension($file);
+		}
+
+		return $extensions;
+	}
+
+	/**
+	 * Gets the file extensions.
+	 *
+	 * @param   string  $file  File to get the extensions from.
+	 *
+	 * @return  array  The file extension.
+	 *
+	 * @throws  BuildException
+	 */
+	private function getFileExtension($file)
+	{
+		$extension = pathinfo($file, PATHINFO_EXTENSION);
+
+		if (!in_array($extension, array('css', 'js')))
+		{
+			throw new BuildException("Invalid file extension ($extension). Only css and js files are supported.");
+		}
+
+		return $extension;
 	}
 }
